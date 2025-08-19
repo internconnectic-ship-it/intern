dae8b66f7d8a4b8eab4606ca51d5b6c1807b97ac
65011211002-svg <internconnect.ic@gmail.com>
2025-08-19 21:15:05 +0700

    first commit

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use('/uploads', express.static('uploads'));
 app.use('/api/internship', internshipRoutes);
 app.use('/api/evaluation', evaluationRoutes);
 app.use('/api/reports', reportRoutes);
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // 📌 log เวลา mount route change-password
 console.log("📌 changePasswordRoutes ถูกโหลดเรียบร้อย");
