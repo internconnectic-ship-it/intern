d55b2942bedfd6357eac0a305da00d56525644ca
65011211002-svg <internconnect.ic@gmail.com>
2025-08-21 00:13:48 +0700

    first commit

--- backend/routes/jobPostRoutes.js
+++ backend/routes/jobPostRoutes.js
@@ -210,14 +210,18 @@ router.get('/applications/:company_id', async (req, res) => {
 
   try {
     const [rows] = await db.promise().query(`
-      SELECT 
-        a.*, s.student_name, s.email, s.phone_number, s.major, s.faculty, jp.position, jp.business_type  
-      FROM application a
-      JOIN job_posting jp ON a.job_posting_id = jp.job_posting_id
-      JOIN student s ON a.student_id = s.student_id
-      WHERE jp.company_id = ?
-      ORDER BY a.apply_date DESC
-    `, [company_id]);
+    SELECT 
+      a.*, 
+      s.student_name, s.email, s.phone_number, s.major, s.faculty, 
+      jp.position, jp.business_type,
+      i.company_id AS confirmed_company_id
+    FROM application a
+    JOIN job_posting jp ON a.job_posting_id = jp.job_posting_id
+    JOIN student s ON a.student_id = s.student_id
+    LEFT JOIN internship i ON a.student_id = i.student_id
+    WHERE jp.company_id = ?
+    ORDER BY a.apply_date DESC
+  `, [company_id]);
 
     res.status(200).json(rows);
   } catch (err) {
