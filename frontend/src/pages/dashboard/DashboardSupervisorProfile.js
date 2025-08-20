6c00d97a899a9d6d14f49250c79dda8756980729
65011211002-svg <internconnect.ic@gmail.com>
2025-08-20 18:28:49 +0700

    first commit

--- frontend/src/pages/dashboard/DashboardSupervisorProfile.js
+++ frontend/src/pages/dashboard/DashboardSupervisorProfile.js
@@ -202,7 +202,7 @@ const DashboardSupervisorProfile = () => {
                 name="currentPassword"
                 value={passwordForm.currentPassword}
                 onChange={handlePasswordChange}
-                autoComplete="off"
+                autoComplete="new-password"
                 className="w-full rounded-xl border border-[#E6F0FF] bg-[#F8FBFF] px-3 py-2"
               />
             </div>
