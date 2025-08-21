551fa4b2440fc43fb3e40a451e250b431479f649
65011211002-svg <internconnect.ic@gmail.com>
2025-08-22 01:20:57 +0700

    first commit

--- backend/routes/evaluationRoutes.js
+++ backend/routes/evaluationRoutes.js
@@ -158,7 +158,7 @@ router.post('/submit', async (req, res) => {
         evaluation_id, student_id, company_id,
         p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,
         w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,
-        absent_sick||0, absent_personal||0, late_days||0, absent_uninformed||0,
+        absent_sick ?? 0, absent_personal??0, late_days??0, absent_uninformed??0,
         company_comment
       ]);
     }
