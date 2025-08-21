16a7733eb49fbe589541e40dce83c954c21e0359
65011211002-svg <internconnect.ic@gmail.com>
2025-08-21 22:08:52 +0700

    first commit

--- backend/server.js
+++ backend/server.js
@@ -14,26 +14,10 @@ const internshipRoutes = require('./routes/internshipRoutes');
 const evaluationRoutes = require('./routes/evaluationRoutes');
 const reportRoutes = require('./routes/reportRoutes');
 const changePasswordRoutes = require('./routes/changePasswordRoutes');
-const allowedOrigins = [
-  "http://localhost:3000",                 // เวลา dev
-  "https://intern-connect160.netlify.app"  // domain ของ Netlify จริง
-];
 require('dotenv').config();
 
 const app = express();
-//app.use(cors());
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+app.use(cors());
 app.use(express.json());
 
 // ✅ กำหนดเส้นทาง API
@@ -48,11 +32,6 @@ app.use('/api/job_posting', jobPostRoutes);
 app.use('/api/internship', internshipRoutes);
 app.use('/api/evaluation', evaluationRoutes);
 app.use('/api/reports', reportRoutes);
-app.options("*", cors());
-app.use((req, res, next) => {
-  console.log("🌐 Request from:", req.headers.origin, req.method, req.path);
-  next();
-});
 
 // ✅ เส้นทางไฟล์ static (สำหรับรูป/ไฟล์ upload)
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
