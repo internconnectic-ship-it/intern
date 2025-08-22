a818a766e72bfb2290a85539ce762119f1bf917e
65011211002-svg <internconnect.ic@gmail.com>
2025-08-22 12:20:42 +0700

    first commit

--- frontend/src/pages/EvaluationCompanyForm.js
+++ frontend/src/pages/EvaluationCompanyForm.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
 const EvaluationCompanyForm = () => {
   const { id } = useParams();
